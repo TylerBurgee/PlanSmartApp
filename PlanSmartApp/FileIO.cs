aeab67b06c0a2a4a53bc073d661f26a21b70ed02
Tyler Burgee <37166272+TylerBurgee@users.noreply.github.com>
2022-10-06 11:09:50 -0400

    Update FileIO.cs

--- PlanSmartApp/FileIO.cs
+++ PlanSmartApp/FileIO.cs
@@ -6,26 +6,27 @@ public class FileIO
 	{
 	}
 
-	/* Save content to .json file*/
-	public static void save_to_file(string filename)
+	/* Creates new .json file*/
+	public static void create_file(string filename)
 	{
 
 	}
-
-    /* Delete sub-dictionary from .json file*/
-    public static void delete_from_file(string filename)
-	{
-
-	}
-
+	
 	/* Create new sub-dictionary within .json file*/
-	public static void add_subdict(string filename, Dictionary<string, string> new_subdict)
+	public static void create_subdict(string filename, Dictionary<string, string> new_subdict)
+	{
+	// sub-dictionary looks like this within the .json file: 
+	//{id1: {key1: value1, key2: value2}, id2: {key1: value1, key2: value2}}
+	}
+
+	/* Delete sub-dictionary from .json file*/
+	public static void delete_subdict(string filename)
 	{
 
 	}
 
-    /* Add content to existing sub-dictionary within .json file*/
-    public static void add_to_subdict(string filename)
+	/* Add new key-pair value to existing sub-dictionary within .json file*/
+	public static void add_to_subdict(string filename)
 	{
 
 	}
