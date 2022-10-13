1ad64e9a2e497041ace6ec15a6d68a007bee01de
Marc Wiand <piratesoda1@gmail.com>
2022-10-13 17:11:21 -0400

    Update fileIO

--- PlanSmartApp/FileIO.cs
+++ PlanSmartApp/FileIO.cs
@@ -9,7 +9,7 @@ public class FileIO
 	/* Creates new, empty .json file*/
 	public static void create_file(string filename)
 	{
-
+		File.Create(filename);
 	}
 	
 	/* Create new sub-dictionary within .json file*/
@@ -17,17 +17,18 @@ public class FileIO
 	{
 	// Sub-dictionaries looks like this within the .json file: 
 	// {id1: {key1: value1, key2: value2}, id2: {key1: value1, key2: value2}}
+	System.IO.File.WriteAllText(filename new_subdict);
 	}
 
 	/* Delete sub-dictionary from .json file*/
 	public static void delete_subdict(string filename)
 	{
-
+		File.Delete(filename);
 	}
 
 	/* Add new key-pair value to existing sub-dictionary within .json file*/
-	public static void add_to_subdict(string filename)
+	public static void add_to_subdict(string filename, Dictionary<string, string> new)
 	{
-
+		new.add(string, string);
 	}
 }
