65b6697a6c6527ed69e6a08a22cbaa3b19fa2ead
Ian O'Brien <IOBRIE01@rams.shepherd.edu>
2022-10-25 13:26:58 -0400

    temp changes to email.cs, outlines what's necessary in a gross monolith of code

--- PlanSmartApp/Email.cs
+++ PlanSmartApp/Email.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 
 public class Email
 {
 	public Email()
 	{
+		NetworkCredential login;
+		SmtpClient client;
+		MailMessage msg;
 	}
 
 	public void sendInvitation(string subject, string body)
@@ -15,4 +22,26 @@ public class Email
     {
         return;
     }
+
+	public void loginMethod(string username, string password)
+	{
+		
+		
+	}
+
+	public void smtpServer(string client, string port)
+	{
+		login = new NetworkCredential(username, password);
+		smtpClientChoice = new SmtpClient(client);
+		stmpClientChoice.Port = Convert.ToInt32(port);
+		stmpClientChoice.EnableSsl = chkSSL.Checked;
+		client.Credentials = Login;
+		msg = new MailMessage { From = new MailAddress(username, "name", Encoding.UTF8)}
+		msg.To.Add(new MailAddress("recipient"))
+		msg.Subject = "subject"
+		msg.Body = "body"
+		msg.BodyEncoding = Encoding.UTF8
+		msg.IsBodyHtml = true;
+		msg.Priority = MailPriority.Normal
+	}
 }
